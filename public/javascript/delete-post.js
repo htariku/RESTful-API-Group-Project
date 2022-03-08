17010efc86c3063cb9d264bb9098d7f7edf776b1
htariku <hgtariku@yahoo.com>
2022-03-07 19:59:39 -0500

    login handlebars update and rendering

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,22 +1,19 @@
-// A function to delete a post
-async function deleteFormHandler(event) {
-    event.preventDefault();
+async function deleteFormHandler(event){
+  event.preventDefault();
 
-    // get the post id from the url
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-        ];
-    // delete the post with an async function
-    const response = await fetch(`/api/posts/${id}`, {
-        method: 'DELETE'
-      });
-    // if the delete action is successful, redirect to the dashboard page, otherwise display the error
-    if (response.ok) {
-        document.location.replace('/dashboard');
-        // otherwise, display the error
-      } else {
-        alert(response.statusText);
-      }
-  }
-  
-  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+  const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+  ];
+
+  await fetch(`/api/posts/${id}`, {
+      method: 'DELETE'
+  });
+
+  if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    } 
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);

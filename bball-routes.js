f61a2c52afdf3074ece98aac51b0b821d787b684
Sohrob1 <94089858+Sohrob1@users.noreply.github.com>
2022-03-08 15:56:14 -0500

    Update bball-routes.js

--- bball-routes.js
+++ bball-routes.js
@@ -3,12 +3,12 @@ const moment = require('moment');
 const { response } = require('express');
 
 
+
 var currentDate = moment().format().toString().split('T');
 const options = {
   method: 'GET',
   url: 'https://api-basketball.p.rapidapi.com/games?',
   qs: {date: currentDate[0]},
-  // parameters: "teams",
   headers: {
     'x-rapidapi-host': 'api-basketball.p.rapidapi.com',
     'x-rapidapi-key': '5c4704c719msh0f08bd1ea31cf24p119c03jsnb8e576c8e250',
@@ -16,18 +16,22 @@ const options = {
   }
 };
 
-request(options, function (error, response, body) {
+var getScores = function() {
+  request(options, function (error, response, body) {
 	if (error) throw new Error(error);
 
 
 	let responseBody = JSON.parse(response.body)
 
-    // console.log(responseBody)
     let responseArr = responseBody.response
     responseArr.forEach((ob)=>{
-      console.log(ob.valueOf())
-      // console.log(ob.teams.home.name);
-      // console.log(ob.teams.away.name);
+      if(ob.league.name==="NBA"){
+       var scoreOb= [ob.teams.home.name, ob.scores.home.total, ob.teams.away.name, ob.scores.away.total]
+       console.log(scoreOb)
+      }
     })
-
 });
+};
+getScores();
+// module.exports=getScores());
+

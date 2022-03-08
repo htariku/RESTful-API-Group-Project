a1439b517ce3213f030b35d3cbe89b5347da1cdc
Brahm Gicheru <brahmgicheru@brahms-mbp.myfiosgateway.com>
2022-03-07 20:59:31 -0500

    updated users, posts, and comments tables

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (user_firstname, user_lastname, user_accountname)
+VALUES
+('Robert', 'Smith', 'rsmith'),
+('Danica', 'Peters', 'dpeters'),
+('James', 'Jenkins', 'jjenkins'),
+('Mark', 'Williams', 'mwilliams'),
+('Michelle', 'Turner', 'mturner'),
+('Amber', 'Harris', 'aharris'),
+('Phil', 'Hayward', 'phayward'),
+('Tom', 'Johnson', 'tjohnson'),
+('Sarah', 'Randles', 'srandles'),
+('Hannah', 'Pierce', 'hpierce');
